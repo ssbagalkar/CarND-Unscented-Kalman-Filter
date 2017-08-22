26afdeca68357c32a77ec7d3d4165757e79c1bcb
Saurabh Bagalkar <sbagalka@asu.edu>
2017-08-22 14:35:08 -0500

    predict sigma points

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -143,8 +143,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  */
 
 
-void UKF::Prediction(double delta_t)
-{
+void UKF::Prediction(double delta_t){
   /**
   TODO:
 
@@ -176,8 +175,50 @@ void UKF::Prediction(double delta_t)
 	}
 
 	///* Let's predict the sigma points
-	//calculate delta time is seconds
-	 
+	//predict sigma points
+	for (int i = 0; i < 2 * n_aug_ + 1; i++)
+	{
+		//extract values for better readability
+		double p_x = Xsig_aug_(0, i);
+		double p_y = Xsig_aug_(1, i);
+		double v = Xsig_aug_(2, i);
+		double yaw = Xsig_aug_(3, i);
+		double yawd = Xsig_aug_(4, i);
+		double nu_a = Xsig_aug_(5, i);
+		double nu_yawdd = Xsig_aug_(6, i);
+
+		//predicted state values
+		double px_p, py_p;
+
+		//avoid division by zero
+		if (fabs(yawd) > 0.001) {
+			px_p = p_x + v / yawd * (sin(yaw + yawd*delta_t) - sin(yaw));
+			py_p = p_y + v / yawd * (cos(yaw) - cos(yaw + yawd*delta_t));
+		}
+		else {
+			px_p = p_x + v*delta_t*cos(yaw);
+			py_p = p_y + v*delta_t*sin(yaw);
+		}
+
+		double v_p = v;
+		double yaw_p = yaw + yawd*delta_t;
+		double yawd_p = yawd;
+
+		//add noise
+		px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+		py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+		v_p = v_p + nu_a*delta_t;
+
+		yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+		yawd_p = yawd_p + nu_yawdd*delta_t;
+
+		//write predicted sigma point into right column
+		Xsig_pred_(0, i) = px_p;
+		Xsig_pred_(1, i) = py_p;
+		Xsig_pred_(2, i) = v_p;
+		Xsig_pred_(3, i) = yaw_p;
+		Xsig_pred_(4, i) = yawd_p;
+	}
 }
 
 /**
